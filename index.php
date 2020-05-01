9d9eaee40a65440a676dd4373e62916cedaa441f
pavelFaraday <devstudio.corp@gmail.com>
2020-05-02 00:49:38 +0400

    add image upload path

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 		<div class="maincontent clear">
 
 <?php 
-	$query = "select * from tbl_post limit 3";
+	$query = "select * from tbl_post limit 10";
 	$post = $db->select($query);
 
 	if ($post) {
@@ -26,7 +26,7 @@
 			<div class="samepost clear">
 				<h2><a href="post.php?id=<?php echo $result['id']; ?>"><?php echo $result['title']; ?></a></h2>
 				<h4><?php echo $fm->formatDate($result['date']); ?>, By <a href="#"><?php echo $result['author']; ?></a></h4>
-				 <a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+				 <a href="#"><img src="admin/upload/<?php echo $result['image'] ?>" alt="post image"/></a>
 
 					<?php echo $fm->textShorten($result['body']); ?>
 
@@ -38,8 +38,7 @@
 	    <?php } ?> <!--End While Loop-->
     <?php } else { header("Location:404.php"); } ?>
 
+</div>
 
-		</div>
-
-		<?php include 'inc/sidebar.php'?>
-		<?php include 'inc/footer.php'?>
+<?php include 'inc/sidebar.php'?>
+<?php include 'inc/footer.php'?>

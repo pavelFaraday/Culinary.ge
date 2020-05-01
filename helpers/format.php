c2a5a51195c04ceea9336b6bd058d296e7a38a58
pavelFaraday <devstudio.corp@gmail.com>
2020-05-02 00:31:01 +0400

    Format Date | Make Text Shorten

---
+++ helpers/format.php
@@ -0,0 +1,17 @@
+<?php
+    // Format
+    class Format
+    {
+        public function formatDate($date) {
+            return date('F j, Y - g:i a', strtotime($date));
+        }
+
+        public function textShorten($text, $limit = 400) {
+            $text = $text. " ";
+            $text = substr($text, 0, $limit);
+            // $text = substr($text, 0, strpos($text, ' '));
+            $text = $text. "...";
+            return $text;
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php include 'config/config.php'?>
 <?php include 'lib/Database.php'?>
 
+<?php include 'helpers/format.php'?>
+
 <?php include 'inc/header.php'?>
 <?php include 'inc/slider.php'?>
 
 <?php 
 	$db = new Database();
+	$fm = new Format();
 ?>
 
 
@@ -13,20 +16,20 @@
 		<div class="maincontent clear">
 
 <?php 
-	$query = "select * from tbl_post";
+	$query = "select * from tbl_post limit 3";
 	$post = $db->select($query);
 
 	if ($post) {
 		while ($result = $post->fetch_assoc()) {
 					
 ?>
-
-
 			<div class="samepost clear">
 				<h2><a href="post.php?id=<?php echo $result['id']; ?>"><?php echo $result['title']; ?></a></h2>
-				<h4><?php echo $result['date']; ?>, By <a href="#"><?php echo $result['author']; ?></a></h4>
+				<h4><?php echo $fm->formatDate($result['date']); ?>, By <a href="#"><?php echo $result['author']; ?></a></h4>
 				 <a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-				 <?php echo $result['body']; ?>
+
+					<?php echo $fm->textShorten($result['body']); ?>
+
 				<div class="readmore clear">
 				<a href="post.php?id=<?php echo $result['id']; ?>">Read More</a>
 				</div>

0735ef649ae75548e37c8a1b65e222afc3954b7b
pavelFaraday <devstudio.corp@gmail.com>
2020-05-02 20:24:21 +0400

    Show related posts

--- post.php
+++ post.php
@@ -14,7 +14,6 @@
 				<?php 
 					$query = "select * from tbl_post where id=$id";
 					$post = $db->select($query);
-					
 					if ($post) {
 						while ($result = $post->fetch_assoc()) {	
 				?>
@@ -22,17 +21,23 @@
 					<h4><?php echo $fm->formatDate($result['date']); ?>, By <a href="#"><?php echo $result['author']; ?></a></h4>
 					<img src="admin/upload/<?php echo $result['image'] ?>" alt="post image"/>
 					<?php echo $result['body'] ?>
-					<?php } } else { header("Location:404.php");} ?>
 
 				<div class="relatedpost clear">
 					<h2>Related articles</h2>
-					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-					<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
+					<?php 
+						$catid = $result['cat'];
+						$queryrelated = "select * from tbl_post where cat='$catid' order by rand() limit 6";
+						$relatedpost = $db->select($queryrelated);
+						if ($relatedpost) {
+							while ($rresult = $relatedpost->fetch_assoc()) {
+					?>
+					
+					<a href="post.php?id=<?php echo $rresult['id']; ?>">
+						<img src="admin/upload/<?php echo $rresult['image'] ?>" alt="post image"/>
+					</a>
+					<?php } } else { echo "No Related Posts Available !!"; } ?>
 				</div>
+				<?php } } else { header("Location:404.php");} ?>
 	    </div>
 	</div>
 

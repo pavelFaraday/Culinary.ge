4e477b3b29736138706d99c1aa4ce23beb4a78ca
pavelFaraday <devstudio.corp@gmail.com>
2020-05-02 19:24:37 +0400

    add Paginations

--- index.php
+++ index.php
@@ -14,14 +14,22 @@
 
 	<div class="contentsection contemplete clear">
 		<div class="maincontent clear">
-
+	<!-- Pagination -->
+	<?php 
+		$per_page = 5;  // for page post limitation change this..
+		if (isset($_GET["page"])) {
+			$page = $_GET["page"];
+		} else {
+			$page = 1;
+		}
+		$start_form = ($page - 1) * $per_page;
+	?>
+	<!-- Pagination -->
 <?php 
-	$query = "select * from tbl_post limit 10";
+	$query = "select * from tbl_post limit $start_form, $per_page";
 	$post = $db->select($query);
-
 	if ($post) {
-		while ($result = $post->fetch_assoc()) {
-					
+		while ($result = $post->fetch_assoc()) {				
 ?>
 			<div class="samepost clear">
 				<h2><a href="post.php?id=<?php echo $result['id']; ?>"><?php echo $result['title']; ?></a></h2>
@@ -36,6 +44,21 @@
 			</div>
 
 	    <?php } ?> <!--End While Loop-->
+
+		<!-- Pagination -->
+		<?php 
+		$query = "select * from tbl_post";
+		$result = $db->select($query);
+		$total_rows = mysqli_num_rows($result);
+		$total_pages = ceil($total_rows/$per_page);
+
+		echo "<span class='pagination'><a href='index.php?page=1'>".'First Page'."</a>";
+		for ($i=1; $i <= $total_pages; $i++) { 
+			echo "<a href='index.php?page=".$i."'>".$i."</a>";
+		}
+		echo "<a href='index.php?page=$total_pages'>".'Last Page'."</a></span>"?>
+		<!-- Pagination -->
+
     <?php } else { header("Location:404.php"); } ?>
 
 </div>

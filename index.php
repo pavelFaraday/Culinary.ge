f62022cb546675d18b47c9992e9ef08b97969b0e
pavelFaraday <devstudio.corp@gmail.com>
2020-05-02 19:55:57 +0400

    Show Post details from DB

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
-<?php include 'config/config.php'?>
-<?php include 'lib/Database.php'?>
-
-<?php include 'helpers/format.php'?>
-
 <?php include 'inc/header.php'?>
 <?php include 'inc/slider.php'?>
-
-<?php 
-	$db = new Database();
-	$fm = new Format();
-?>
-
 
 	<div class="contentsection contemplete clear">
 		<div class="maincontent clear">

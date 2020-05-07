6f257890b513c9c27caea40b909cae870b6104ea
pavelFaraday <devstudio.corp@gmail.com>
2020-05-07 13:41:47 +0400

    Add Categories From CMS

--- admin/addcat.php
+++ admin/addcat.php
@@ -1,29 +1,47 @@
 ﻿<?php include 'inc/header.php'?>
 <?php include 'inc/sidebar.php'?>
 
-        <div class="grid_10">
-            <div class="box round first grid">
-                <h2>Add New Category</h2>
-               <div class="block copyblock"> 
-                 <form>
-                    <table class="form">					
-                        <tr>
-                            <td>
-                                <input type="text" placeholder="Enter Category Name..." class="medium" />
-                            </td>
-                        </tr>
-						<tr> 
-                            <td>
-                                <input type="submit" name="submit" Value="Save" />
-                            </td>
-                        </tr>
-                    </table>
-                    </form>
-                </div>
+<div class="grid_10">
+    <div class="box round first grid">
+        <h2>Add New Category</h2>
+        <div class="block copyblock">
+             
+<?php 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $name = mysqli_real_escape_string($db->link, $name);
+    if (empty($name)) {
+        echo "<span class='error'>Field must not be empty !</span>";
+    } else {
+        $query = "INSERT INTO tbl_category(name) VALUES('$name')";
+        $catinsert = $db->insert($query);
+        if ($catinsert) {
+            echo "<span class='success'>Category added Successfully</span>";
+        } else {
+            echo "<span class='error'>ERROR! Category could not be added !</span>";
+        }
+    }
+}
+?>
+            <form action="" method="post">
+                <table class="form">					
+                    <tr>
+                        <td>
+                            <input type="text" name="name" placeholder="Enter Category Name..." class="medium" />
+                        </td>
+                    </tr>
+                    <tr> 
+                        <td>
+                            <input type="submit" name="submit" Value="Save" />
+                        </td>
+                    </tr>
+                </table>
+            </form>
             </div>
         </div>
-        <div class="clear">
-        </div>
     </div>
+    <div class="clear">
+    </div>
+</div>
 
 <?php include 'inc/footer.php'?>
